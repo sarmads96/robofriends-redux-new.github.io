33fa603211188b40d3ea0c29de61d5116f31ef43
sarmadsohail <sarmad.sohail@cloudtek.biz>
2023-01-13 20:07:17 +0500

    added robofriends to createReactApp

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import 'tachyons';
+import { requestRobots, searchRobots } from './reducers'
+const logger = createLogger() 
+
+const rootReducers = combineReducers({requestRobots, searchRobots})
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
